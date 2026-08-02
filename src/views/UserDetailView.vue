f265009840047e1dad0084ac44644d4591468ff3
kaungkaung <kaunghtoo764@gmail.com>
2026-08-02 22:04:21 +0700

    finished 6.3 lab3

---
+++ src/views/UserDetailView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import type { User } from '@/types'
+import UserService from '@/services/UserService'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+
+const user = ref<User | null>(null)
+
+onMounted(() => {
+  UserService.getUser(Number(props.id))
+    .then((response) => {
+      user.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <div v-if="user">
+    <h1>{{ user.name }}</h1>
+    <p>{{ user.email }}</p>
+    <p>{{ user.company.name }}</p>
+  </div>
+</template>
